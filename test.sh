755c8b552b4332a1280bdab3b1f6c37535de153b
Jonas Gefele <github.com@schrieb.de>
2018-01-18 23:45:15 +0100

    next try

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ if [[ "$TRAVIS" == "" ]]; then
     INTERACTIVE_NB_DIFF=--nbdime;
 fi
 ulimit -a
-python -c "import librenderman as rm; import utils; dexed=utils.download_plugins(); e = rm.RenderEngine(44100,512,512); rc = e.load_plugin(dexed); assert(rc)" || exit $?
+strace python -c "import librenderman as rm; import utils; dexed=utils.download_plugins(); e = rm.RenderEngine(44100,512,512); rc = e.load_plugin(dexed); assert(rc)" || exit $?
 py.test --nbval $INTERACTIVE_NB_DIFF || exit $?
