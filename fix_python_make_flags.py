f8973de5044b2f2915b7b95aa2039c7e4579f881
Jonas Gefele <github.com@schrieb.de>
2018-01-22 09:26:40 +0100

    Travis CI: fix 'sed' for Linux and debug OSX failure

--- fix_python_make_flags.py
+++ fix_python_make_flags.py
@@ -13,8 +13,8 @@ include_list=include_flags.replace("-I", "").split()
 from sys import platform
 if platform == "linux" or platform == "linux2":
   linux_makefile="Builds/LinuxMakefile/Makefile"
-  subprocess.check_call(["sed", "-i", "", 's@-I/usr/include/python2.7@'+include_flags+'@g', linux_makefile])
-  subprocess.check_call(["sed", "-i", "", "s@-lpython2.7@"+linker_flags+"@g", linux_makefile])
+  subprocess.check_call(["sed", "-i", 's@-I/usr/include/python2.7@'+include_flags+'@g', linux_makefile])
+  subprocess.check_call(["sed", "-i", "s@-lpython2.7@"+linker_flags+"@g", linux_makefile])
 elif platform == "darwin":
   xcode_project="Builds/MacOSX/RenderMan.xcodeproj/project.pbxproj"
   xcode_includes=' '.join(['"%s",'%i for i in include_list])
--- test.sh
+++ test.sh
@@ -5,5 +5,12 @@ cd "$(dirname "$0")"/Documentation
 if [[ "$TRAVIS" == "" ]]; then
     INTERACTIVE_NB_DIFF=--nbdime;
 fi
-python -c "import librenderman as rm; import utils; dexed=utils.download_plugins(); print(dexed); e = rm.RenderEngine(44100,512,512); rc = e.load_plugin(dexed); assert(rc)" || exit $?
+python --version
+python2 --version
+which python
+which python2
+which pip2
+ls -l /usr/bin
+ls -l /usr/local/bin
+python2 -c "import librenderman as rm; import utils; dexed=utils.download_plugins(); print(dexed); e = rm.RenderEngine(44100,512,512); rc = e.load_plugin(dexed); assert(rc)" || exit $?
 py.test --nbval $INTERACTIVE_NB_DIFF || exit $?
